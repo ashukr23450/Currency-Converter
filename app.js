e066eb3cfe24820ebb6b9134008903ce96083394
ashukr23450 <ashukr23450@gmail.com>
2025-01-17 21:29:25 +0530

    slecting country applied

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/
 const dropdowns = document.querySelectorAll(".dropdown select");
 
 for (let select of dropdowns) {
+    console.log(select)
+
     for (currCode in countryList) {
     let newOption = document.createElement("option");
     newOption.innerText = currCode;
     newOption.value = currCode;
     select.append(newOption);
+
+    
+    // Always select country applying
+        if (select.name==="from"  && currCode === "USD" ){
+            newOption.selected = " selected"
+        }else if (select.name==="To"  && currCode === "INR" ){
+            newOption.selected = " selected"
+        }
     }
 }
