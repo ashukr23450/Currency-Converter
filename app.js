d5e99b396524b97b1cab6db3aa7e889a22b122d8
ashukr23450 <ashukr23450@gmail.com>
2025-01-18 09:59:11 +0530

    btn work prevent

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json" ;
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
 
 for (let select of dropdowns) {
-    console.log(select)
-
     for (currCode in countryList) {
     let newOption = document.createElement("option");
     newOption.innerText = currCode;
@@ -28,9 +27,21 @@ const updateFlag = (element) =>{
     let currCode = element.value;
     console.log(currCode)
     let countryCode = countryList[currCode];
-    console.log(countryCode)
+    // console.log(countryCode)
     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png` ;
     let img = element.parentElement.querySelector("img");
     img.src = newSrc ;
-
 }
+
+btn.addEventListener("click" , (evt)=>{
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+
+    if (amtVal === "" || amtVal < 1){
+        amtVal = 1 ;
+        amount.value = "1" ;
+    }
+    
+})
+
