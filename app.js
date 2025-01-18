1c2728345ecd2bfacf094228fee05a6789d13327
ashukr23450 <ashukr23450@gmail.com>
2025-01-18 17:44:57 +0530

    re design

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json" ;
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies" ;
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select") ;
+const toCurr = document.querySelector(".to select") ;
 
 for (let select of dropdowns) {
     for (currCode in countryList) {
@@ -25,23 +27,10 @@ for (let select of dropdowns) {
 
 const updateFlag = (element) =>{
     let currCode = element.value;
-    console.log(currCode)
+    console.log(currCode);
     let countryCode = countryList[currCode];
-    // console.log(countryCode)
     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png` ;
     let img = element.parentElement.querySelector("img");
     img.src = newSrc ;
-}
-
-btn.addEventListener("click" , (evt)=>{
-    evt.preventDefault();
-    let amount = document.querySelector(".amount input");
-    let amtVal = amount.value;
-
-    if (amtVal === "" || amtVal < 1){
-        amtVal = 1 ;
-        amount.value = "1" ;
-    }
     
-})
-
+}
